7b85d746357bec65d43825cf2bddeca2169b0e7a
Travis <traviseengland565@gmail.com>
2020-01-06 00:34:11 -0500

    Second commit updating bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""Overal class to manage game assets and behavior."""
@@ -19,6 +20,7 @@ class AlienInvasion:
 		pygame.display.set_caption("Alien Invasion")
 
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 
 
@@ -27,7 +29,10 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
+			self._update_bullets()
 			self._update_screen()
+
+
 
 
 	def _check_events(self):
@@ -43,6 +48,8 @@ class AlienInvasion:
 		#Updates images on the screen, and flip to the new screen
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 
 		pygame.display.flip()
 
@@ -55,6 +62,8 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 
 	def _check_keyup_events(self, event):
@@ -63,6 +72,19 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
 
+	def _fire_bullet(self):
+		#Create a new bullet and add it to the bullets group
+		if len(self.bullets) < self.settings.bullets_allowed:
+		    new_bullet = Bullet(self)
+		    self.bullets.add(new_bullet)
+
+	def _update_bullets(self):
+		#Update bullets positions
+		self.bullets.update()
+			#Get rid of bullets that have disappeared
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
 
 
 if __name__ == '__main__':
