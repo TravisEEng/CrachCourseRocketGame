ee915abb2fb998805806746a4a8dead90d9ac4c1
Travis <traviseengland565@gmail.com>
2020-01-07 19:38:44 -0500

    Adjusted gitignore added more code

---
+++ game_stats.py
@@ -0,0 +1,15 @@
+class GameStats:
+	#Track stats for Alien invasion
+
+	def __init__(self,ai_game):
+		#Initialize statistics.
+		self.settings = ai_game.settings
+		self.reset_stats()
+		#Start game in active state
+		self.game_active = True
+
+	def reset_stats(self):
+		#Initalize statistics that can change during the game.
+		self.ships_left = self.settings.ship_limit
+
+		
